db09f9c17f5a640d375e9c153131f9e276243115
Mike Schertl <schert@hm.edu>
2020-04-21 23:51:42 +0200

    update mock

---
+++ src/utils/video-mock.js
@@ -0,0 +1,145 @@
+module.exports = [
+  {
+    channelId: "UC3kbAe8BguiZFnsVYY1oW0w",
+    channelTitle: "Steve Cook",
+    channelThumbnailUrl:
+      "https://yt3.ggpht.com/-R3_gXLISSyM/AAAAAAAAAAI/AAAAAAAAAAA/3jk4U4-kjN8/s88-c-k-no-mo-rj-c0xffffff/photo.jpg",
+    videos: [
+      {
+        id: "WaSECdqSu_o",
+        title: "MY MORNING FITNESS ROUTINE",
+        description:
+          "Download the Fitness Culture App below ‍♂️ ----------------- ▷Fitness Culture 30-Day Challenge: https://bit.ly/FC30DayChallenge ▷Fitness Culture ...",
+        thumbnail: {
+          url: {
+            url: "https://i.ytimg.com/vi/WaSECdqSu_o/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+        },
+      },
+      {
+        id: "xDnucK_lXDo",
+        title:
+          "WHAT I EAT IN A DAY // Quarantine Style // Lean Muscle Building Macros",
+        description:
+          "Download the Fitness Culture App below ‍♂️ ----------------- ▷Fitness Culture 30-Day Challenge: https://bit.ly/FC30DayChallenge ▷Fitness Culture ...",
+        thumbnail: {
+          url: {
+            url: "https://i.ytimg.com/vi/xDnucK_lXDo/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+        },
+      },
+      {
+        id: "HcvnPM3p750",
+        title: "ULTIMATE AT-HOME Leg Challenge // KOALA CHALLENGE Redo",
+        description:
+          "Download the Fitness Culture App below ‍♂️ ----------------- ▷Fitness Culture 30-Day Challenge: https://bit.ly/FC30DayChallenge ▷Fitness Culture ...",
+        thumbnail: {
+          url: {
+            url: "https://i.ytimg.com/vi/HcvnPM3p750/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+        },
+      },
+    ],
+  },
+  {
+    channelId: "UCU1iJ2ChGwaNLvBjip0p2Ag",
+    channelTitle: "Christian Guzman",
+    channelThumbnailUrl:
+      "https://yt3.ggpht.com/-4uC1ZZThkh8/AAAAAAAAAAI/AAAAAAAAAAA/q7DtLnCIbpA/s88-c-k-no-mo-rj-c0xffffff/photo.jpg",
+    videos: [
+      {
+        id: "pQP5kerlTdY",
+        title: "THE BIG SURPRISE!!!",
+        description:
+          'Alphalete: https://alphaleteathletics.com/?rfsn=1678822.810f98 ➢ Ghost Supplements: 20% off code "CG" http://tinyurl.com/gpxc69v ➢ MVMT Watches: 15% off ...',
+        thumbnail: {
+          url: {
+            url: "https://i.ytimg.com/vi/pQP5kerlTdY/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+        },
+      },
+      {
+        id: "a40svCMWPXE",
+        title: "The Downfall of Alphalete",
+        description:
+          'Alphalete: https://alphaleteathletics.com/?rfsn=1678822.810f98 ➢ Ghost Supplements: 20% off code "CG" http://tinyurl.com/gpxc69v ➢ MVMT Watches: 15% off ...',
+        thumbnail: {
+          url: {
+            url: "https://i.ytimg.com/vi/a40svCMWPXE/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+        },
+      },
+      {
+        id: "Z3-zWqo7_60",
+        title: "THE ULTIMATE GIFT",
+        description:
+          'Alphalete: https://alphaleteathletics.com/?rfsn=1678822.810f98 ➢ Ghost Supplements: 20% off code "CG" http://tinyurl.com/gpxc69v ➢ MVMT Watches: 15% off ...',
+        thumbnail: {
+          url: {
+            url: "https://i.ytimg.com/vi/Z3-zWqo7_60/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+        },
+      },
+    ],
+  },
+  {
+    channelId: "UC-b3c7kxa5vU-bnmaROgvog",
+    channelTitle: "The Futur",
+    channelThumbnailUrl:
+      "https://yt3.ggpht.com/-H4tzOveBPrs/AAAAAAAAAAI/AAAAAAAAAAA/twYvlcrb0OI/s88-c-k-no-mo-rj-c0xffffff/photo.jpg",
+    videos: [
+      {
+        id: "ZkQM-WBeRjc",
+        title: "Who Are You Making Content For? Know Your Audience",
+        description:
+          "How to profile your ideal audience through empathy (task +gap). Find and define your audience and make content for them. Seek to deliver value first. The test: ...",
+        thumbnail: {
+          url: {
+            url: "https://i.ytimg.com/vi/ZkQM-WBeRjc/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+        },
+      },
+      {
+        id: "ZbFak7vZvVQ",
+        title:
+          "Why You Should Use New Features To Exploit The Instagram Algorithm (4 min video)",
+        description:
+          "How do you get more attention on Instagram? How does the algorithm work? Why is it good that there are more platforms available to creators right now?",
+        thumbnail: {
+          url: {
+            url: "https://i.ytimg.com/vi/ZbFak7vZvVQ/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+        },
+      },
+      {
+        id: "1JqdZ-WRP6U",
+        title: "Ten Dangerous Ideas with Graphic Design Rebel James Victore",
+        description:
+          "James Victore is a dangerous man that uses strong language, expressive typography and lettering to communicate his opinions. James Victore is an author, ...",
+        thumbnail: {
+          url: {
+            url: "https://i.ytimg.com/vi/1JqdZ-WRP6U/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+        },
+      },
+    ],
+  },
+];
