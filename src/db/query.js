fe59c78ec619ced24244ea37af4b2799e60a90f8
Mike Schertl <schert@hm.edu>
2020-04-21 23:52:11 +0200

    add thumbail for db request

--- src/db/query.js
+++ src/db/query.js
@@ -1,1 +1,8 @@
 const sequelize = require("sequelize");
+const Channel = require("./models");
+
+exports.getAllChannelsFromDb = async () => {
+  return await Channel.findAll({
+    attributes: ["channelId", "channelTitle", "thumbnail"],
+  });
+};
